fb0b954e695b696fe5c468909c8bf53be474986e
unknown <nadhemb@yahoo.com>
2024-01-20 10:01:45 +0100

    first commit

---
+++ src/main/java/com/nadhem/users/restControllers/UserRestController.java
@@ -0,0 +1,23 @@
+package com.nadhem.users.restControllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.nadhem.users.entities.User;
+import com.nadhem.users.service.UserService;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class UserRestController {
+	@Autowired
+	UserService userService;
+
+	@GetMapping("all")
+	public List<User> getAllUsers() {
+		return userService.findAllUsers();
+	}
+
+}
+
